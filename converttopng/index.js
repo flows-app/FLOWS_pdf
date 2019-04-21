8dc1f5957aa83c5713d2c625759509b39828f9cb
Jens Walter <jens@apimeister.com>
2019-04-21 22:30:36 +0200

    use imagemagick for pdf conversion

--- converttopng/index.js
+++ converttopng/index.js
@@ -1,18 +1,11 @@
-const PDFImage = require("pdf-image").PDFImage;
 const FS = require("fs");
+const CHILD = require("child_process");
 
 exports.handler = (event, context, callback) => {
-  console.log('event');
-  console.log(JSON.stringify(event,null,2));
-  console.log('context');
-  console.log(JSON.stringify(context,null,2));
-
   FS.writeFileSync("/tmp/tmp.pdf", event.base64 , 'base64');
-  let pdfImage = new PDFImage("/tmp/tmp.pdf");
 
-  pdfImage.convertPage(0).then((paths)=>{
-    let buf = FS.readFileSync(paths);
-    let result = {image: buf.toString('base64')};
-    callback(null,result);
-  });
+  CHILD.execSync("convert -density 300 -append /tmp/tmp.pdf /tmp/tmp.png");
+  let buf = FS.readFileSync("/tmp/tmp.png");
+  let result = {image: buf.toString('base64')};
+  callback(null,result);
 }
