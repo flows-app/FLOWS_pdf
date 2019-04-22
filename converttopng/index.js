bce5b03675b46f66f8db347b7c6cb8d35274a368
Jens Walter <jens@apimeister.com>
2019-04-22 18:15:56 +0200

    increase memory for performance

--- converttopng/index.js
+++ converttopng/index.js
@@ -4,7 +4,7 @@ const CHILD = require("child_process");
 exports.handler = (event, context, callback) => {
   FS.writeFileSync("/tmp/tmp.pdf", event.base64 , 'base64');
 
-  CHILD.execSync("convert -density 300 -append /tmp/tmp.pdf /tmp/tmp.png");
+  CHILD.execSync("convert -density 300 -append -flatten /tmp/tmp.pdf /tmp/tmp.png");
   let buf = FS.readFileSync("/tmp/tmp.png");
   let result = {image: buf.toString('base64')};
   callback(null,result);
